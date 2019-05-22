876d21c5fedd6a47dc8699b162e3abfd2a303c89
Patricio Napoli <pnapoli@geminis.io>
2019-05-21 21:21:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kingduck-ai
+
+
---
+++ src/main/java/KingDuci.java
@@ -0,0 +1,22 @@
+import ia.battle.core.ConfigurationManager;
+import ia.battle.core.Warrior;
+import ia.battle.core.WarriorManager;
+import ia.exceptions.RuleException;
+
+public class KingDuci extends WarriorManager {
+
+	@Override
+	public String getName() {
+		return "King Duci";
+	}
+
+	@Override
+	public Warrior getNextWarrior() throws RuleException {
+		
+		int maxPoints = ConfigurationManager.getInstance().getMaxPointsPerWarrior();
+		
+		
+		return new Duci("Duci", 10, 10, 10, 10, 10);
+	}
+
+}
